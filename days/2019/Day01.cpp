fd7c173f1957208b303ca2fa9284a3a4c0e0b022
Rafq77 <sokol-77@wp.pl>
2019-11-13 17:26:28 +0100

    Test commit

--- days/2019/Day01.cpp
+++ days/2019/Day01.cpp
@@ -4,6 +4,7 @@ namespace year2019 {
 
 Day01::Day01() 
 {
+    // read from file
     //Day("day_01.txt");
 
 }
--- test/Year2019Tests.cpp
+++ test/Year2019Tests.cpp
@@ -1,11 +1,36 @@
 #include <gtest/gtest.h>
 #include "../days/2019/Day01.hpp"
 
+#include <fstream>
+
 TEST(Year2019, Day01_Test) 
 {
     year2019::Day01 day;
-    EXPECT_EQ("138", day.getResultOfPart1());
+    //day(Input::fromRaw("(())()")); // return this
+
+    EXPECT_EQ("137", day.getResultOfPart1());
     //EXPECT_EQ(1771, day.getResultOfPart2());
 
     //EXPECT_EQ(false, ff()) << " but value " << gValue2;
+}
+
+TEST(Year2019, FileIO)
+{
+    std::fstream stream;
+    stream.open("./ttt.txt", std::fstream::out);
+    stream << "Hello File\n";
+    stream.close();
+}
+
+TEST(Year2019, FileReadInputFromRelativePath)
+{
+    std::ifstream stream;
+    std::string filePath("../../days/2019/input/day01.txt");
+    stream.open(filePath);
+    std::string tmp;
+    stream >> tmp;
+    stream.close();
+
+    std::cout << tmp;
+    EXPECT_EQ(1,2); // todo compare beginning
 }
