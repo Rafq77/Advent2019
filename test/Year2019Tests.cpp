797b78381631f2311ddba7ad414220ca9b11babe
Rafq77 <sokol-77@wp.pl>
2019-11-14 16:59:55 +0100

    Adjust last test

--- test/Year2019Tests.cpp
+++ test/Year2019Tests.cpp
@@ -2,6 +2,10 @@
 #include "../days/2019/Day01.hpp"
 
 #include <fstream>
+#include <cstdint>
+#include <string_view>
+
+using namespace std::string_view_literals;
 
 TEST(Year2019, Day01_Test) 
 {
@@ -25,12 +29,15 @@ TEST(Year2019, FileIO)
 TEST(Year2019, FileReadInputFromRelativePath)
 {
     std::ifstream stream;
-    std::string filePath("../../days/2019/input/day01.txt");
+    std::string filePath("../../days/2019/input/day01.txt"sv);
     stream.open(filePath);
     std::string tmp;
     stream >> tmp;
     stream.close();
 
-    std::cout << tmp;
-    EXPECT_EQ(1,2); // todo compare beginning
+    const uint16_t numberOfCharsInFile = 7000;
+    const std::string_view firstTenChars = "()(((()))("sv;
+
+    EXPECT_EQ(numberOfCharsInFile ,tmp.size());
+    EXPECT_STREQ(std::string(firstTenChars).c_str(), tmp.substr(0, 10).c_str());
 }
