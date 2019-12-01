24570e8913fff472aec1f7a2d166f44d0a160375
Rafq77 <sokol-77@wp.pl>
2019-12-01 11:21:59 +0100

    2019/1 Day 1

--- days/2019/Day01.cpp
+++ days/2019/Day01.cpp
@@ -4,6 +4,8 @@
 #include <numeric>
 #include <set>
 
+#include <range/v3/all.hpp>
+
 namespace year2019 {
 
 template <class T>
@@ -15,14 +17,25 @@ Day01<T>::Day01()
 template <class T>
 void Day01<T>::solvePart1() 
 {
-    part1Solution = std::accumulate(input.begin(), input.end(), 0 );
+    // For a mass of X, divide by 3 and round down to get Y, then subtract 2 to get RESULT.
+    part1Solution = std::accumulate(input.begin(), input.end(), 0 , [](int32_t sum, int32_t x){ return sum + (x/3 - 2); } );
+
     //ranges::accumulate(lengths, 0l) / 2;
 }
 
 template <class T>
 void Day01<T>::solvePart2() 
 {
-    // std::set while double entry not applied
+    part2Solution = std::accumulate(input.begin(), input.end(), 0 , [](int32_t sum, int32_t x){ 
+        int32_t fuelReq = x;
+        int32_t totalFuelReq = 0;
+        while (fuelReq > 0)
+        {
+            fuelReq = (fuelReq/3 - 2); 
+            totalFuelReq += (fuelReq < 0 ) ? 0 : fuelReq;
+        }
+        return sum + totalFuelReq;
+    });
 }
 
 }
--- test/Year2019.cpp
+++ test/Year2019.cpp
@@ -14,44 +14,21 @@ TEST(Year2019, Day01_Test)
 {
     year2019::Day01<int32_t> daySolver(Input<int32_t>::fromFile("../../days/2019/input/day01.txt"));
 
-    EXPECT_EQ(540, daySolver.getResultOfPart1());
-    //EXPECT_EQ(73056, daySolver.getResultOfPart2());
+    EXPECT_EQ(3159380, daySolver.getResultOfPart1());
+    EXPECT_EQ(4736213, daySolver.getResultOfPart2());
 }
 
 TEST(Year2019, Day01_Part1_Examples)
 {
-    EXPECT_EQ(3, year2019::Day01<int32_t>(Input{1, -2, +3, +1}).getResultOfPart1());
-    EXPECT_EQ(3, year2019::Day01<int32_t>(Input{+1, +1, +1}).getResultOfPart1());
-    EXPECT_EQ(0, year2019::Day01<int32_t>(Input{+1, +1, -2}).getResultOfPart1());
-    EXPECT_EQ(-6, year2019::Day01<int32_t>(Input{-1, -2, -3}).getResultOfPart1());
+    EXPECT_EQ(2, year2019::Day01<int32_t>(Input{12}).getResultOfPart1());
+    EXPECT_EQ(2, year2019::Day01<int32_t>(Input{14}).getResultOfPart1());
+    EXPECT_EQ(654, year2019::Day01<int32_t>(Input{1969}).getResultOfPart1());
+    EXPECT_EQ(33583, year2019::Day01<int32_t>(Input{100756}).getResultOfPart1());
 }
 
 TEST(Year2019, Day01_Part2_Examples)
 {
-    /*
-    +1, -1 first reaches 0 twice.
-+3, +3, +4, -2, -4 first reaches 10 twice.
--6, +3, +8, +5, -6 first reaches 5 twice.
-+7, +7, -2, -7, -4 first reaches 14 twice.
-
-For example, using the same list of changes above, the device would loop as follows:
-
-Current frequency  0, change of +1; resulting frequency  1.
-Current frequency  1, change of -2; resulting frequency -1.
-Current frequency -1, change of +3; resulting frequency  2.
-Current frequency  2, change of +1; resulting frequency  3.
-(At this point, the device continues from the start of the list.)
-Current frequency  3, change of +1; resulting frequency  4.
-Current frequency  4, change of -2; resulting frequency  2, which has already been seen.
-*/
-}
-
-TEST(Year2019, InputStreamToIntFromFile)
-{
-    std::string fileName{"../../days/2019/input/day01.txt"};
-    std::ifstream infile(fileName);
-    auto tmpInput = std::vector<int32_t>(std::istream_iterator<int32_t>(infile),{});
-
-    EXPECT_EQ(-19, tmpInput.front());
-    EXPECT_EQ(-73375, tmpInput.back());
+    EXPECT_EQ(2, year2019::Day01<int32_t>(Input{14}).getResultOfPart2());
+    EXPECT_EQ(966, year2019::Day01<int32_t>(Input{1969}).getResultOfPart2());
+    EXPECT_EQ(50346, year2019::Day01<int32_t>(Input{100756}).getResultOfPart2());
 }
